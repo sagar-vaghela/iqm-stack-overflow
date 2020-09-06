efc5790abc49909aba49370000acd596ecc22cc7
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2020-09-06 23:46:30 +0530

    change in gitignore and semicolon issue

--- src/questions/QuestionStore.ts
+++ src/questions/QuestionStore.ts
@@ -1,14 +1,14 @@
 export interface QuestionQuery {
-    limit: number
-    offset: number
+    limit: number;
+    offset: number;
 }
 
 export interface QuestionData {
-    author: string
-    title: string
-    creationDate: number
-    body: string
-    link: string
+    author: string;
+    title: string;
+    creationDate: number;
+    body: string;
+    link: string;
 }
 
 export type QuestionStore = QuestionData[];
